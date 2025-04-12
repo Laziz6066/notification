6c89202da0236b4c37c5f2ac04b2bdf0ff85b813
Lazik <lazizsaidov95@gmail.com>
2025-04-12 10:02:31 +0500

    change admin keyboards

--- app/keyboards/admin_keyboards.py
+++ app/keyboards/admin_keyboards.py
@@ -4,11 +4,15 @@ from app.config import ADMINS, ADMIN
 
 async def get_main_keyboard(user_id: int) -> ReplyKeyboardMarkup:
 
+    buttons = []
+
     if user_id in ADMINS:
-        buttons = [
+        buttons.extend([
             [KeyboardButton(text='Просмотр')],
             [KeyboardButton(text='Приемка')],
-        ]
-        if user_id == ADMIN:
-            buttons.insert(__index=1, __object=[KeyboardButton(text='Добавить')])
-        return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+        ])
+
+    if ADMIN and user_id == ADMIN:
+        buttons.insert(1, [KeyboardButton(text='Добавить')])
+
+    return ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
